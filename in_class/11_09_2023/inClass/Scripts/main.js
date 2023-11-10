ea6bdda60a34b4100de65e719b2fd6972bb4e76e
mkieselhorst <89440801+mkieselhorst@users.noreply.github.com>
2023-11-09 19:03:27 -0500

    js thingies

--- in_class/11_09_2023/inClass/Scripts/main.js
+++ in_class/11_09_2023/inClass/Scripts/main.js
@@ -1,1 +1,18 @@
-alert("linked");
+// alert("linked");
+//Keyword name (args) {function}
+var num1 = 10, num2 = 20;
+sayHello();
+calculate(num1, num2);
+var total = returnSomething(5, 6);
+function sayHello() {
+    document.write("My First JS function");
+
+}
+function calculate(num1, num2){
+    document.write("<p>num1 + num2 = " + (num1+num2)+ "</p>");
+}
+function returnSomething(num1, num2){
+    return(num1 * num2);
+    
+}
+document.write( "<p>num1 * num2 = " + total + "</p>");
