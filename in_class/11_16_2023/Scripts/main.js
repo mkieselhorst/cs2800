f04977f631aea787c1772995c7915149f37f3ffd
mkieselhorst <89440801+mkieselhorst@users.noreply.github.com>
2023-11-16 19:30:38 -0500

    add/sub

--- in_class/11_16_2023/Scripts/main.js
+++ in_class/11_16_2023/Scripts/main.js
@@ -1,12 +1,28 @@
-function addNums(){
+// function addNums(){
+//     var num1 = document.getElementById("boxOne").value;
+//     var num2 = document.getElementById("boxTwo").value;
+//     num1 = parseFloat(num1);
+//     num2 = parseFloat(num2);
+    
+//     if(isNaN(num1) || isNaN(num2) ){
+//         alert('Not valid number(s)');
+//     }
+//     var sum = (num1 + num2);
+//     document.getElementById('realResult').innerText = "Result: " + sum;
+// }
+function calculate(option){
     var num1 = document.getElementById("boxOne").value;
     var num2 = document.getElementById("boxTwo").value;
     num1 = parseFloat(num1);
     num2 = parseFloat(num2);
-    
     if(isNaN(num1) || isNaN(num2) ){
         alert('Not valid number(s)');
     }
-    var sum = (num1 + num2);
-    document.getElementById('realResult').innerText = "Result: " + sum;
+    var result;
+    if(option === 'add'){
+        result = (num1 + num2);
+    }else if(option === 'subtract'){
+        result = (num1 - num2);
+    }
+    document.getElementById('realResult').innerText = "Result: " + result;
 }
