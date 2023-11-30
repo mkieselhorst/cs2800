3ccc065782907f6c95fc798558f4e6131aa3973f
mkieselhorst <89440801+mkieselhorst@users.noreply.github.com>
2023-11-30 08:41:08 -0500

    fin

--- in_class/11_28_2023/scripts/script.js
+++ in_class/11_28_2023/scripts/script.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
 // hide all answers using jquery
     $('dd').hide();
-
+    
     // Accordion affect using jquery ui
     $('dt').on('click', function(){
         $(this).next('dd').slideToggle();
-        // $('dd').not($(this).next('dd'));
+        
     })
 
+    function slideImages(){
+        $("#image-container img:first-child").appendTo("#image-container").hide().show("slide", {direction: "right"}, 1000);
+    }
+    setInterval(slideImages, 1000);
+    
 });
